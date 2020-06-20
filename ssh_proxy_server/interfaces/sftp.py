d25fc3e59866267763e8c852033dcc5bb9bc4f55
Manfred Kaiser <manfred.kaiser@logfile.at>
2020-06-20 12:45:51 +0200

    added sftp file transfer

--- ssh_proxy_server/interfaces/sftp.py
+++ ssh_proxy_server/interfaces/sftp.py
@@ -8,21 +8,13 @@ class StubSFTPHandle(paramiko.SFTPHandle):
         super().__init__(flags)
         self.writefile = None
         self.readfile = None
-        self.filename = None
 
-    def stat(self):
-        f = self.writefile if self.writefile else self.readfile
-        try:
-            return paramiko.SFTPAttributes.from_stat(os.fstat(f.fileno()))
-        except OSError as e:
-            return paramiko.SFTPServer.convert_errno(e.errno)
+    def read(self, offset, length):
+        return self.readfile.read(length)
 
-    def chattr(self, attr):
-        try:
-            paramiko.SFTPServer.set_file_attr(self.filename, attr)
-            return paramiko.SFTP_OK
-        except OSError as e:
-            return paramiko.SFTPServer.convert_errno(e.errno)
+    def write(self, offset, data):
+        self.writefile.write(data)
+        return paramiko.SFTP_OK
 
 
 class SFTPProxyServerInterface(paramiko.SFTPServerInterface):
@@ -55,88 +47,48 @@ class SFTPProxyServerInterface(paramiko.SFTPServerInterface):
         return self.session.sftp_client.mkdir(path, attr.st_mode)
 
     def open(self, remotePath, flags, attr):
-        if not os.path.exists(self.ROOT):
-            os.mkdir(self.ROOT)
+        try:
+            self.session.sftp_client_ready.wait()
 
-        localPath = os.path.join(
-            self.ROOT,
-            ''.join([
-                self.session.getUniquePrefix(),
-                os.path.basename(remotePath)
-            ])
-        )
-        self.session.clientReady.wait()
-
-        # Code aus dem StubSFTPServer der Paramiko Demo auf Github
-        if (flags & os.O_CREAT) and attr:
-            attr._flags &= ~attr.FLAG_PERMISSIONS
-            paramiko.SFTPServer.set_file_attr(localPath, attr)
-        if flags & os.O_WRONLY:
-            if flags & os.O_APPEND:
-                fstr = 'ab'
+            # Code aus dem StubSFTPServer der Paramiko Demo auf Github
+            if (flags & os.O_CREAT) and attr:
+                attr._flags &= ~attr.FLAG_PERMISSIONS
+                paramiko.SFTPServer.set_file_attr(remotePath, attr)
+            if flags & os.O_WRONLY:
+                if flags & os.O_APPEND:
+                    fstr = 'ab'
+                else:
+                    fstr = 'wb'
+            elif flags & os.O_RDWR:
+                if flags & os.O_APPEND:
+                    fstr = 'a+b'
+                else:
+                    fstr = 'r+b'
             else:
-                fstr = 'wb'
-        elif flags & os.O_RDWR:
-            if flags & os.O_APPEND:
-                fstr = 'a+b'
-            else:
-                fstr = 'r+b'
-        else:
-            # O_RDONLY (== 0)
-            fstr = 'rb'
+                # O_RDONLY (== 0)
+                fstr = 'rb'
 
-        # alle Werte von 'fstr' außer 'wb' haben Leserechte auf die Datei
-        # dh. wir müssen die Datei downloaden
-        if fstr != 'wb':
             try:
-                self.session.sftpClient.get(remotePath, localPath)
-            except (IOError, OSError) as ex:
-                logging.error('SFTP get failed!')
-                logging.error(ex)
-                return paramiko.SFTPServer.convert_errno(ex.errno)
+                client_f = self.session.sftp_client._sftp.open(remotePath, fstr)
+            except Exception:
+                logging.exception("Error file")
+                return None
 
-        try:
-            binaryFlag = getattr(os, 'O_BINARY', 0)
-            flags |= binaryFlag
-            mode = getattr(attr, 'st_mode', None)
-
-            if mode:
-                fd = os.open(localPath, flags, mode)
-            else:
-                # os.open() defaults to 0777 which is
-                # an odd default mode for files
-                fd = os.open(localPath, flags, 0o666)
-        except OSError as ex:
-            logging.error(ex)
-            if not self.session.proxyserver.config.getboolean("SFTP", "keep_files"):
-                os.remove(localPath)
-            return paramiko.SFTPServer.convert_errno(ex.errno)
-
-        try:
-            f = os.fdopen(fd, fstr)
-            fobj = StubSFTPHandle(flags)
-            fobj.remotePath = remotePath
-
-            fobj.filename = localPath
-
-            # die Standardimplementation des SFTPHandles verwendet 'writefile'
-            # und 'readfile' um zu lesen bzw. zu schreiben.
-            # Wir verwenden diese Klassenmember, um zwischen readonly,
-            # writeonly und read and write Dateien zu unterscheiden
+            fobj = StubSFTPHandle(client_f)
 
             # writeonly
             if fstr in ('wb', 'ab'):
-                fobj.writefile = f
+                fobj.writefile = client_f
             # readonly
             elif fstr == 'rb':
-                fobj.readfile = f
+                fobj.readfile = client_f
             # read and write
             elif fstr in ('a+b', 'r+b'):
-                fobj.writefile = f
-                fobj.readfile = f
+                fobj.writefile = client_f
+                fobj.readfile = client_f
             return fobj
-        except OSError as e:
-            logging.error(e)
+        except Exception as e:
+            logging.exception("Error")
             return paramiko.SFTPServer.convert_errno(e.errno)
 
     def readlink(self, path):
