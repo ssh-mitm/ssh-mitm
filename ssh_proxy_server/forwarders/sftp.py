b37febfccfd7102a70f3f119effedeed45c19aba
Simon Böhm <simon-boehm@kabsi.at>
2020-11-30 11:10:48 +0100

    SFTPHandlerReplacePlugin fixed buffer problem

--- ssh_proxy_server/forwarders/sftp.py
+++ ssh_proxy_server/forwarders/sftp.py
@@ -1,3 +1,4 @@
+import math
 import uuid
 import os
 import logging
@@ -8,9 +9,10 @@ from paramiko import SFTPFile
 
 class SFTPHandlerBasePlugin(Module):
 
-    def __init__(self, filename):
+    def __init__(self, sftp, filename):
         super().__init__()
         self.filename = filename
+        self.sftp = sftp
 
     def close(self):
         pass
@@ -33,8 +35,8 @@ class SFTPHandlerStoragePlugin(SFTPHandlerPlugin):
             help='directory to store files from scp'
         )
 
-    def __init__(self, filename):
-        super().__init__(filename)
+    def __init__(self, sftp, filename):
+        super().__init__(sftp, filename)
         self.file_id = str(uuid.uuid4())
         logging.info("sftp file transfer: %s -> %s", filename, self.file_id)
         self.output_path = os.path.join(self.args.sftp_storage_dir, self.file_id)
@@ -52,7 +54,7 @@ class SFTPBaseHandle(paramiko.SFTPHandle):
 
     def __init__(self, plugin, filename, flags=0):
         super().__init__(flags)
-        self.plugin = plugin(filename)
+        self.plugin = plugin(self, filename)
         self.writefile = None
         self.readfile = None
 
@@ -83,19 +85,28 @@ class SFTPHandlerReplacePlugin(SFTPHandlerPlugin):
             help='file that is used for replacement'
         )
 
-    def __init__(self, filename):
-        super().__init__(filename)
+    def __init__(self, sftp, filename):
+        super().__init__(sftp, filename)
         logging.info("sftp file transfer detected: %s", filename)
         logging.info("intercepting sftp file, replacement: %s", self.args.sftp_replacement_file)
+        self.file = None
         self.replacement = open(self.args.sftp_replacement_file, "rb")
+        self.buf_size = None
 
     def close(self):
         self.replacement.close()
 
     def handle_data(self, data):
-        # Cannot reasonably detect given buffer size, using default 32768
-        # User setting custom buffer size (-B XXX) will get an error, communication will fail
+        # Cannot reasonably detect given buffer size, using best guess: biggest power of 2
         # PUT cannot replace file correctly if the to-be PUT file is smaller than the to-be REPLACED file
         # --> closes file before everything can be transmitted
-        return self.replacement.read(32768)
+        if not self.buf_size and not self.file:
+            self.file = self.sftp.writefile if self.sftp.readfile is None else self.sftp.readfile
+            if len(data) == 0:
+                self.buf_size = 1
+            elif len(data) == self.file.stat().st_size:
+                self.buf_size = pow(2, int(math.log(len(data)) // math.log(2)))
+            else:
+                self.buf_size = len(data)
+        return self.replacement.read(self.buf_size)
 
