38ddd15488d16d5a5c21e3b1464ee87d43853137
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2021-01-21 09:52:09 +0100

    remove "give a star" for long_description

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from setuptools import setup, find_packages
 
 # read the contents of your README file
 from os import path
+import re
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+    long_description = re.sub(r"## Give a Star.*?Thanks!$", "", f.read(), 0, re.DOTALL)
 
 
 setup(
