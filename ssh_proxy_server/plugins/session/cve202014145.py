786c8fcbc640202954632c89d9c1b8d43cd61713
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2021-01-26 11:33:11 +0100

    code cleanup

--- ssh_proxy_server/plugins/session/cve202014145.py
+++ ssh_proxy_server/plugins/session/cve202014145.py
@@ -19,7 +19,7 @@ DEFAULT_ALGORITMS = [
         'rsa-sha2-512-cert-v01@openssh.com', 'rsa-sha2-256-cert-v01@openssh.com',
         'ssh-rsa-cert-v01@openssh.com', 'ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384',
         'ecdsa-sha2-nistp521', 'ssh-ed25519', 'rsa-sha2-512', 'rsa-sha2-256', 'ssh-rsa'
-     ],
+    ],
     [  # client version: OpenSSH_7.2p1 - OpenSSH_7.7p1 Ubuntu-4ubuntu0.3, OpenSSL 1.0.2n  7 Dec 2017
         'ecdsa-sha2-nistp256-cert-v01@openssh.com', 'ecdsa-sha2-nistp384-cert-v01@openssh.com',
         'ecdsa-sha2-nistp521-cert-v01@openssh.com', 'ssh-ed25519-cert-v01@openssh.com',
@@ -33,7 +33,7 @@ DEFAULT_ALGORITMS = [
         'ecdsa-sha2-nistp521', 'ssh-ed25519', 'ssh-rsa'
     ],
     [  # client version: OpenSSH_6.5p1 - OpenSSH_6.9p1
-        'ecdsa-sha2-nistp256-cert-v01@openssh.com','ecdsa-sha2-nistp384-cert-v01@openssh.com',
+        'ecdsa-sha2-nistp256-cert-v01@openssh.com', 'ecdsa-sha2-nistp384-cert-v01@openssh.com',
         'ecdsa-sha2-nistp521-cert-v01@openssh.com', 'ssh-ed25519-cert-v01@openssh.com',
         'ssh-rsa-cert-v01@openssh.com', 'ssh-dss-cert-v01@openssh.com', 'ssh-rsa-cert-v00@openssh.com',
         'ssh-dss-cert-v00@openssh.com', 'ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp521',
