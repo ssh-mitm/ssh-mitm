ea16a8a2f2eec1aee3353b28d73f2d8f75e79bc9
Manfred Kaiser <manfred.kaiser@bmlv.gv.at>
2021-02-10 15:14:54 +0100

    set version to 0.3.25

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_entry_points():
 
 setup(
     name='ssh-mitm',
-    version='0.3.24',
+    version='0.3.25',
     author='SSH-MITM Dev-Team',
     author_email='support@ssh-mitm.at',
     description='ssh mitm server for security audits supporting public key authentication, session hijacking and file manipulation',
