40e86e7a62c58fac5891d1d2387d9b19ba20e776
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2021-07-14 15:21:58 +0200

    reorganized gui

--- ssh_proxy_server/gui.py
+++ ssh_proxy_server/gui.py
@@ -45,9 +45,16 @@ from ssh_proxy_server.workarounds import dropbear
 from ssh_proxy_server.plugins.ssh.mirrorshell import SSHMirrorForwarder
 from ssh_proxy_server.__version__ import version as ssh_mitm_version
 
-from gooey import Gooey
+from gooey import Gooey, GooeyParser
 
-@Gooey
+
+@Gooey(
+    program_name='SSH-MITM {}'.format(ssh_mitm_version),
+    program_description='ssh man in the middle (ssh-mitm) server for security audits',
+    tabbed_groups=True,
+    optional_cols=1,
+    default_size=(610, 590),
+)
 def main():
 
     if os.environ.get('APPIMAGE', None):
@@ -55,60 +62,74 @@ def main():
         if len(sys.argv) == 2 and sys.argv[-1] == '':
             sys.argv = sys.argv[:-1]
 
-    parser = ModuleParser(description='SSH Proxy Server', modules_from_file=True)
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    logformatter = logging.Formatter('%(asctime)s [%(levelname)s]  %(message)s')
+    for handler in root_logger.handlers:
+        handler.setFormatter(logformatter)
+    logging.getLogger("paramiko").setLevel(logging.WARNING)
 
-    parser.add_plugin(LogModule)
 
-    parser.add_argument(
+    parser = GooeyParser(description='SSH Proxy Server')
+
+    basicsettings = parser.add_argument_group("Basic settings")
+    basicsettings.add_argument(
+        '--banner-name',
+        metavar='SSH banner',
+        dest='banner_name',
+        default='SSHMITM_{}'.format(ssh_mitm_version),
+        help='set a custom string as server banner'
+    )
+    basicsettings.add_argument(
         '--listen-port',
+        metavar='listen port',
         dest='listen_port',
         default=10022,
         type=int,
         help='listen port'
     )
-    parser.add_argument(
-        '--host-key',
-        dest='host_key',
-        help='host key file'
+    basicsettings.add_argument(
+        '--request-agent',
+        metavar='request agent',
+        dest='request_agent',
+        action='store_true',
+        help='request agent for public key authentication'
     )
-    parser.add_argument(
+
+    hostkeysettings = parser.add_argument_group("Server host key")
+    hostkeysettings.add_argument(
+        '--host-key',
+        metavar='host key file (optional)',
+        dest='host_key',
+        help='host key file',
+        widget="FileChooser"
+    )
+    hostkeysettings.add_argument(
         '--host-key-algorithm',
+        metavar='type of host key',
         dest='host_key_algorithm',
         default='rsa',
         choices=['dss', 'rsa', 'ecdsa', 'ed25519'],
         help='host key algorithm (default rsa)'
     )
-    parser.add_argument(
+    hostkeysettings.add_argument(
         '--host-key-length',
+        metavar='host key length',
         dest='host_key_length',
         default=2048,
         type=int,
         help='host key length for dss and rsa (default 2048)'
     )
-    parser.add_argument(
-        '--request-agent',
-        dest='request_agent',
-        action='store_true',
-        help='request agent for public key authentication'
-    )
-    parser.add_argument(
-        '--banner-name',
-        dest='banner_name',
-        default='SSHMITM_{}'.format(ssh_mitm_version),
-        help='set a custom string as server banner'
-    )
 
     args = parser.parse_args()
 
+    Transport._CLIENT_ID = args.banner_name
     Transport.run = dropbear.transport_run
-
-    logging.getLogger("paramiko").setLevel(logging.WARNING)
 
     authenticator = AuthenticatorPassThrough
     authenticator.REQUEST_AGENT = args.request_agent
 
-    logging.info("starting SSH-MITM %s", ssh_mitm_version)
-    proxy = SSHProxyServer(
+    SSHProxyServer(
         args.listen_port,
         key_file=args.host_key,
         key_algorithm=args.host_key_algorithm,
@@ -123,10 +144,7 @@ def main():
         authenticator=authenticator,
         transparent=False,
         args=args
-    )
-    if args.banner_name is not None:
-        Transport._CLIENT_ID = args.banner_name
-    proxy.start()
+    ).start()
 
 
 if __name__ == '__main__':
