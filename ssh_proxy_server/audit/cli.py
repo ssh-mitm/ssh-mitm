6f5dec48f631e02ecf0080095f0de5c0a8a20e58
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2021-09-10 18:51:23 +0200

    added subcommands to ssh-mitm-audit

--- ssh_proxy_server/audit/cli.py
+++ ssh_proxy_server/audit/cli.py
@@ -8,14 +8,7 @@ from paramiko.pkey import PublicBlob
 from ssh_proxy_server.authentication import probe_host
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('host', type=str, help='Hostname or IP address')
-    parser.add_argument('--port', type=int, default=22)
-    parser.add_argument('--username', type=str, required=True)
-    parser.add_argument('--public-key', type=str, required=True)
-
-    args = parser.parse_args(sys.argv[1:])
+def check_publickey(args):
     key = open(args.public_key, 'rt').read()
     if probe_host(
         hostname_or_ip=args.host,
@@ -26,3 +19,18 @@ def main():
         print("valid key")
     else:
         print("bad key")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='Available commands', dest="subparser_name", metavar='subcommand')
+    subparsers.required = True
+    check_publickey = subparsers.add_parser('check-publickey', help='checks a username and publickey against a server')
+    check_publickey.add_argument('--host', type=str, required=True, help='Hostname or IP address')
+    check_publickey.add_argument('--port', type=int, default=22, help='port (default: 22)')
+    check_publickey.add_argument('--username', type=str, required=True, help='username to check')
+    check_publickey.add_argument('--public-key', type=str, required=True, help='publickey to check')
+
+    args = parser.parse_args(sys.argv[1:])
+    if args.subparser_name == 'check-publickey':
+        check_publickey(args)
