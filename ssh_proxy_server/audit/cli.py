986af2d591586544030d6b448d778f752b311d06
Manfred Kaiser <manfred.kaiser@bmlv.gv.at>
2021-09-15 14:10:45 +0200

    fixed check_publickey command

--- ssh_proxy_server/audit/cli.py
+++ ssh_proxy_server/audit/cli.py
@@ -25,11 +25,12 @@ def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title='Available commands', dest="subparser_name", metavar='subcommand')
     subparsers.required = True
-    check_publickey = subparsers.add_parser('check-publickey', help='checks a username and publickey against a server')
-    check_publickey.add_argument('--host', type=str, required=True, help='Hostname or IP address')
-    check_publickey.add_argument('--port', type=int, default=22, help='port (default: 22)')
-    check_publickey.add_argument('--username', type=str, required=True, help='username to check')
-    check_publickey.add_argument('--public-key', type=str, required=True, help='publickey to check')
+
+    parser_check_publickey = subparsers.add_parser('check-publickey', help='checks a username and publickey against a server')
+    parser_check_publickey.add_argument('--host', type=str, required=True, help='Hostname or IP address')
+    parser_check_publickey.add_argument('--port', type=int, default=22, help='port (default: 22)')
+    parser_check_publickey.add_argument('--username', type=str, required=True, help='username to check')
+    parser_check_publickey.add_argument('--public-key', type=str, required=True, help='publickey to check')
 
     args = parser.parse_args(sys.argv[1:])
     if args.subparser_name == 'check-publickey':
