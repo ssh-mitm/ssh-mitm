05590eee964aa830ffca24cdfb861a72188cde5e
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2022-02-07 14:59:56 +0100

    updated supported python versions

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Topic :: System :: Networking",
         "Development Status :: 4 - Beta"
