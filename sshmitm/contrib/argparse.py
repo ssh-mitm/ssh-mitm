6e5fe6efa2926a223241183d00213ac5ff724d3c
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2022-08-11 15:44:48 +0200

    fixed function name

--- sshmitm/contrib/argparse.py
+++ sshmitm/contrib/argparse.py
@@ -10,7 +10,7 @@ from enhancements.modules import ModuleParser
 
 class SshMitmParser(ModuleParser):
 
-    def _create_parser(self, args: Optional[Sequence[Text]] = None, namespace: Optional[argparse.Namespace] = None) -> 'argparse.ArgumentParser':
+    def create_parser(self, args: Optional[Sequence[Text]] = None, namespace: Optional[argparse.Namespace] = None) -> 'argparse.ArgumentParser':
         parser = super()._create_parser(args, namespace)
         argcomplete.autocomplete(parser)
         return parser
