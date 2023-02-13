a7065bc63b84eb9954460e45aa6952d207d659ea
Manfred Kaiser <manfred.kaiser@bmlv.gv.at>
2023-02-13 08:00:45 +0100

    added some documentation

--- sshmitm/plugins/session/clientaudit.py
+++ sshmitm/plugins/session/clientaudit.py
@@ -26,6 +26,15 @@ if TYPE_CHECKING:
 
 
 class Vulnerability:
+    """
+    This class represents a vulnerability and holds information about it.
+
+    :param cve: the identifier of the vulnerability (e.g. 'CVE-2022-0001')
+    :type cve: str
+    :param indocs: if True, the URL of the vulnerability information will point to the internal docs.
+                  if False, the URL will point to the official NIST National Vulnerability Database.
+    :type indocs: bool
+    """
 
     def __init__(self, cve: str, indocs: bool = False) -> None:
         self.cve: str = cve
@@ -33,12 +42,32 @@ class Vulnerability:
 
     @property
     def url(self) -> str:
+        """
+        Get the URL where the information about the vulnerability can be found.
+
+        :return: the URL
+        :rtype: str
+        """
         if self.indocs:
             return f"https://docs.ssh-mitm.at/vulnerabilities/{self.cve}.html"
         return f"https://nvd.nist.gov/vuln/detail/{self.cve}"
 
 
 class SSHClientAudit():
+    """
+    The class SSHClientAudit is used for auditing SSH clients.
+
+    :param key_negotiation_data: object of 'sshmitm.plugins.session.key_negotiation.KeyNegotiationData'
+    :type key_negotiation_data: 'sshmitm.plugins.session.key_negotiation.KeyNegotiationData'
+    :param client_version: client version string
+    :type client_version: str
+    :param client_name: optional client name
+    :type client_name: Optional[str]
+    :param client_info: optional client information, stored as a dictionary
+    :type client_info: Optional[Dict[str, Dict[str, Any]]]
+    :return: None
+    :rtype: None
+    """
 
     def __init__(
         self,
@@ -55,6 +84,13 @@ class SSHClientAudit():
         self.vendor_url: Optional[str] = cast(str, self.client_info.get('url', ""))
 
     def get_version_string(self) -> Optional[str]:
+        """
+        This method returns version string extracted from the `client_version` string in the `key_negotiation_data` object
+        using the `version_regex` field of `client_info` dictionary.
+
+        :return: version string
+        :rtype: Optional[str]
+        """
         for version_regex in self.client_info.get('version_regex', []):
             version_sring = re.match(version_regex, self.key_negotiation_data.client_version.lower())
             if version_sring:
@@ -62,6 +98,17 @@ class SSHClientAudit():
         return None
 
     def between_versions(self, version_min: Union[None, int, float, str], version_max: Union[None, int, float, str]) -> bool:
+        """
+        This method returns `True` if the version string is between `version_min` and `version_max`.
+        Returns `False` otherwise.
+
+        :param version_min: minimum version number
+        :type version_min: Union[None, int, float, str]
+        :param version_max: maximum version number
+        :type version_max: Union[None, int, float, str]
+        :return: `True` if version string is between `version_min` and `version_max`, `False` otherwise
+        :rtype: bool
+        """
         try:
             version_string = self.get_version_string()
             if not version_string:
@@ -77,6 +124,15 @@ class SSHClientAudit():
             return False
 
     def check_cves(self, vulnerabilities: Dict[str, List[str]]) -> List[str]:
+        """
+        This method returns a list of strings representing the Common Vulnerabilities and Exposures (CVEs) found in the client,
+        along with the information available in the `vulnerabilities` dictionary.
+
+        :param vulnerabilities: dictionary of CVEs and their descriptions
+        :type vulnerabilities: Dict[str, List[str]]
+        :return: list of strings representing the CVEs and their information
+        :rtype: List[str]
+        """
         cvelist: Dict[str, Vulnerability] = {}
         for cve, description in self.client_info.get('vulnerabilities', {}).items():
             version_min = description.get('version_min', "")
@@ -98,6 +154,12 @@ class SSHClientAudit():
         return cvemessagelist
 
     def _find_known_server_host_key_algos(self) -> List[str]:
+        """
+        This method returns a list of strings representing the server host key algorithms known to the client.
+
+        :return: list of strings representing server host key algorithms
+        :rtype: List[str]
+        """
         messages: List[str] = []
         for client_name, server_host_key_algorithms_list in SERVER_HOST_KEY_ALGORITHMS.items():
             if not isinstance(server_host_key_algorithms_list, list):
@@ -120,6 +182,14 @@ class SSHClientAudit():
         return messages
 
     def _check_known_clients(self, client_name: str) -> List[str]:
+        """
+        Check if a client with the given ID is already registered as a known client.
+
+        :param client_id: ID of the client to check
+        :type client_id: str
+        :return: True if the client is known, False otherwise
+        :rtype: bool
+        """
         messages: List[str] = []
         if client_name not in SERVER_HOST_KEY_ALGORITHMS:
             return self._find_known_server_host_key_algos()
@@ -144,6 +214,14 @@ class SSHClientAudit():
         return messages
 
     def check_key_negotiation(self) -> Dict[str, List[str]]:
+            """
+        Check if a key negotiation data is known.
+
+        :param client_id: ID of the client to check
+        :type client_id: str
+        :return: True if key negotiation data is known, False otherwise
+        :rtype: bool
+        """
         if isinstance(self.key_negotiation_data.session.proxyserver.host_key, ECDSAKey):
             logging.warning("%s: ecdsa-sha2 key is a bad choice; this will produce false positives!", self.client_info.get('name', ''))
 
@@ -158,6 +236,14 @@ class SSHClientAudit():
         return {'clientaudit': messages}
 
     def run_audit(self) -> None:
+        """
+        Run an audit on the client with the given ID.
+
+        :param client_id: ID of the client to audit
+        :type client_id: str
+        :return: None
+        :rtype: None
+        """
         vulnerabilities: DefaultDict[str, List[str]] = defaultdict(list)
         for k, v in self.check_key_negotiation().items():
             vulnerabilities[k].extend(v)
@@ -192,4 +278,10 @@ class SSHClientAudit():
         logging.info("%s", "\n".join(log_output))
 
     def audit(self) -> List[str]:
+        """
+        Run audits on all clients.
+
+        :return: None
+        :rtype: None
+        """
         return []
--- sshmitm/plugins/session/tcpserver.py
+++ sshmitm/plugins/session/tcpserver.py
@@ -1,3 +1,23 @@
+"""
+A Python module for a TCP server that runs as a thread and accepts incoming connections.
+
+This module contains the TCPServerThread class, which implements a basic TCP server that listens for incoming connections and launches a new thread for each connection request.
+
+Example usage:
+
+.. code-block::
+
+    from sshmitm.plugins.session.tcpserver import TCPServerThread
+
+    def handle_request(server_address, client_socket, client_address):
+        print("Received connection from", client_address)
+        client_socket.sendall(b"Hello, client!")
+        client_socket.close()
+
+    server = TCPServerThread(request_handler=handle_request, port=1234)
+    server.start()
+"""
+
 import select
 import socket
 import threading
@@ -13,6 +33,22 @@ import paramiko
 
 
 class TCPServerThread(threading.Thread):
+
+    """
+    A TCP server thread that accepts incoming connections and launches a new thread for each connection.
+
+    :param request_handler: a function to be called for each connection request
+    :type request_handler: Optional[Callable[Tuple[str, int], Union[socket.socket, paramiko.Channel], Tuple[str, int]], None]
+    :param network: network address for the server to bind to
+    :type network: str
+    :param port: port number for the server to listen on
+    :type port: int
+    :param run_status: whether the server should run or not
+    :type run_status: bool
+    :param daemon: whether the server should run as a daemon
+    :type daemon: bool
+    :return: None
+    """
 
     def __init__(
         self,
@@ -40,6 +76,11 @@ class TCPServerThread(threading.Thread):
         self.threads: List[threading.Thread] = []
 
     def run(self) -> None:
+        """
+        Start the server thread and continuously check for incoming connections.
+
+        :return: None
+        """
         while self.running:
             readable = select.select([self.socket], [], [], 0.5)[0]
             if len(readable) == 1 and readable[0] is self.socket:
@@ -49,10 +90,24 @@ class TCPServerThread(threading.Thread):
             time.sleep(0.1)
 
     def handle_request(self, client: Union[socket.socket, paramiko.Channel], addr: Tuple[str, int]) -> None:
+        """
+        Call the handle request callback for a new connection.
+
+        :param client: The client's socket or paramiko channel.
+        :type client: Union[socket.socket, paramiko.Channel]
+        :param addr: Tuple containing the address information of the client.
+        :type addr: Tuple[str, int]
+        :return: None
+        """
         if self.handle_request_callback is not None:
             self.handle_request_callback((self.network, self.port), client, addr)
 
     def close(self) -> None:
+        """
+        Join all the active threads and close the server socket.
+
+        :return: None
+        """
         for t in self.threads:
             t.join()
         self.socket.close()
