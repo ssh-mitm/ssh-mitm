565c80735b14c3fec029986b15952c2b786515a8
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2026-06-24 10:03:08 +0200

    document CVE202229154

--- sshmitm/plugins/scp/cve202229154.py
+++ sshmitm/plugins/scp/cve202229154.py
@@ -10,7 +10,26 @@ from sshmitm.forwarders.scp import SCPForwarder
 
 
 class CVE202229154(SCPForwarder):
-    """inject additional files in rsync"""
+    """Injects an additional file path into rsync commands (CVE-2022-29154).
+
+    When an rsync client syncs files over SSH, this plugin rewrites the rsync
+    server command to append an extra file path.  As a result, the rsync server
+    sends the attacker-controlled file to the client in addition to the originally
+    requested content.
+
+    **Usage example**
+
+    ::
+
+        ssh-mitm server --scp-forwarder CVE-2022-29154 --rsync-inject-file /path/to/inject.txt
+
+    **Notes**
+
+    * Only rsync commands (those starting with ``rsync --server``) are rewritten;
+      all other SCP or exec traffic passes through unchanged.
+    * The injected file path is appended to the rsync argument list; rsync on the
+      server side then includes that path in the transfer.
+    """
 
     @classmethod
     def parser_arguments(cls) -> None:
