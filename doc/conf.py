b4b5bdc4b638679b204be640929b81a0e9bf4a2f
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2026-06-26 19:47:45 +0200

    doc: reduce OG description length to 120 characters

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,7 @@ html_extra_path = ["robots.txt", "llms.txt", "_static/codeberg-logo_icon_blue.sv
 # -- OpenGraph ---------------------------------------------------------------
 
 ogp_site_url = "https://docs.ssh-mitm.at/"
-ogp_description_length = 150
+ogp_description_length = 120
 ogp_image = "https://docs.ssh-mitm.at/_static/intro-og.png"
 ogp_type = "website"
 ogp_enable_meta_description = True
